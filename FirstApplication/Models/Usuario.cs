c2a8b3a1a3dd2197ee2c8824b2cdff585c75e015
Saulo Carvalho <saulo.carv@hotmail.com>
2018-05-29 21:01:27 -0300

    Criando a classe usuario, Criando a model do usuario, criando um construtor que instancia uma nova lista de usuarios baseados na usuario model - criando a classe usuario - construtor da classe usuario: criando uma lista com a criacao de um usuario; - criando metodos de criar, atualizar,buscar e deletar

---
+++ FirstApplication/Models/Usuario.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstApplication.Models
+{
+    public class Usuario
+    {
+        public List<UsuarioModel> listaUsuarios = new List<UsuarioModel>();
+
+        public Usuario()
+        {
+            listaUsuarios.Add(new UsuarioModel
+            {
+                nome = "'Aluno1'",
+                sobrenome = "'Aluno 2'",
+                endereco = "'Rua 111, 100'",
+                email = "'Teste@teste.com'",
+                nascimento = Convert.ToDateTime("'05/09/1975'")
+            });
+        }
+
+        public void CriaUsuario(UsuarioModel usuarioModelo)
+        {
+            listaUsuarios.Add(usuarioModelo);
+        }
+
+        public void AtualizaUsuario(UsuarioModel usuarioModelo)
+        {
+            foreach (UsuarioModel usuario in listaUsuarios)
+            {
+                if(usuario.email == usuarioModelo.email)
+                {
+                    listaUsuarios.Remove(usuario);
+                    listaUsuarios.Add(usuarioModelo);
+                    break;
+                }
+            }
+        }
+
+        public UsuarioModel GetUsuario(string Email)
+        {
+            UsuarioModel _usuarioModel = null;
+            foreach (UsuarioModel _usuario in listaUsuarios)
+                 if (_usuario.email == Email)
+                    _usuarioModel = _usuario;
+
+                return _usuarioModel;
+        }
+
+        public void DeletarUsuario(string Email)
+        {
+            foreach (UsuarioModel _usuario in listaUsuarios)
+            {
+                if(_usuario.email == Email)
+                {
+                    listaUsuarios.Remove(_usuario);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ FirstApplication/Models/UsuarioModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstApplication.Models
+{
+    public class UsuarioModel
+    {
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public string endereco { get; set; }
+        public string email { get; set; }
+        public DateTime nascimento { get; set; }
+    }
+}
